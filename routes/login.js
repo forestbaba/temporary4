4ace5451aab8bc4ba52cfb801e94765e981bc2b4
Adeoy3 <hawlahethan@gmail.com>
2017-10-27 04:02:09 -0700

    changing login from email to username

--- routes/login.js
+++ routes/login.js
@@ -18,10 +18,18 @@ exports.login = function(username,password,callback) {
             var newpass = temp + password;
             var hashed_password = crypto.createHash('sha512').update(newpass).digest("hex");
             var grav_url = gravatar.url(username, {s: '200', r: 'pg', d: '404'});
-            var firstname = users[0].fname
+            var firstname = users[0].fname;
+            var lastname = users[0].lname;
+            var phone = users[0].phone;
+            var email = users[0].email;
+            var username = users[0].username
             if(hash_db == hashed_password){
 
-                callback({'response':"Login Success",'res':true,'token':id,'grav':grav_url,'firstname':firstname});
+                callback({'response':"Login Success",'res':true,'token':id,'grav':grav_url,
+                    'firstname':firstname,'lastname':lastname,'Phone':phone,
+                    'Email':email,
+                    'Username':username
+                });
 
             }
             else
